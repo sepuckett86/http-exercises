005482b5aff00bb498c270cb9a863cc726108061
sepuckett86 <sepuckett86@gmail.com>
2019-06-26 15:08:40 -0700

    add paths and tests

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-const { createServer } = require('http');
+const app = require('./lib/app');
 
-const server = createServer((req, res) => {
-  console.log(req.method);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Animals</title>
-    <style>
-      h1 {
-        color: green;
-      }
-      ul {
-        color: blue;
-      }
-      body {
-        background: lightblue;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Animals</h1>
-    <ul>
-      <li>Sloth</li>
-      <li>Manatee</li>
-      <li>Horse</li>
-    </ul>
-    <script>
-      console.log('hi');
-    </script>
-  </body>
-  </html>`);
-});
-
-server.listen(7890, () => {
+app.listen(7890, () => {
   console.log('Listening on port 7890');
 })
---
+++ tests/app.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('application routes', () => {
+  it('returns a header with welcome', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.text).toEqual(expect.stringContaining('welcome'));
+      });
+  });
+  it('returns a color text', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.text).toEqual(expect.stringContaining('red'));
+      });
+  });
+  it('returns a color text', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.text).toEqual(expect.stringContaining('blue'));
+      });
+  });
+  it('returns a color text', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.text).toEqual(expect.stringContaining('green'));
+      });
+  });
+});
+
+
