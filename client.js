2f9b8c0a747b4c8487b4482d5833ec488d011f53
sepuckett86 <sepuckett86@gmail.com>
2019-06-27 14:53:58 -0700

    finish

--- client.js
+++ client.js
@@ -2,11 +2,18 @@ const { createConnection } = require('net');
 
 const client = createConnection(7890, 'localhost', () => {
   console.log('connected');
-  client.write('hello');
-});
+  // Upon connection, send Hello!
+  client.write('Hello!');
 
+  // Send data to server every second
+  setInterval(() => {
+    client.write(new Date().toISOString());
+  }, 1000)
 
-client.on('data', data => {
-  console.log(data.toString());
-});
+  // console.log data from server
+  client.on('data', data => {
+    console.log('from server', data.toString());
+  })
+});  
+
 
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const { createServer } = require('net');
 
 const server = createServer(sock => {
   console.log('client connected');
-  sock.write('Hello client!');
 
+  // When client connects, send message to client
+  sock.write('I will echo!');
+
+  // When data comes from client, console.log it and send it back
   sock.on('data', data => {
     console.log('from client', data.toString());
+    sock.write(data.toString());
   })
 });
 
